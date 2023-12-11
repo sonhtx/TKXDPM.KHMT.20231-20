4a4303f8df06fe952e733472a3fa65899238de52
DucThang06072001 <103400921+DucThang06072001@users.noreply.github.com>
2023-12-11 23:39:19 +0700

    data coupling BaseController.java

--- AIMS-Codebase/src/main/java/controller/BaseController.java
+++ AIMS-Codebase/src/main/java/controller/BaseController.java
@@ -17,6 +17,7 @@ public class BaseController {
      * The method checks whether the Media in Cart, if it were in, we will return the CartMedia else return null
      * @param media
      * @return CartMedia or null
+     * Data coupling
      */
     public CartMedia checkMediaInCart(Media media){
         return Cart.getCart().checkMediaInCart(media);
@@ -25,6 +26,7 @@ public class BaseController {
     /**
      * This method gets the list of items in cart
      * @return List[CartMedia]
+     * Data coupling
      */
     public List getListCartMedia(){
         return Cart.getCart().getListMedia();
