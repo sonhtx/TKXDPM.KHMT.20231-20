846c74fadd8db0873d39f7b8890dc71b50abdcfc
DucThang06072001 <103400921+DucThang06072001@users.noreply.github.com>
2023-12-21 00:03:21 +0700

    conhesion trong ViewCartController.java

--- AIMS-Codebase/src/main/java/controller/ViewCartController.java
+++ AIMS-Codebase/src/main/java/controller/ViewCartController.java
@@ -15,6 +15,8 @@ public class ViewCartController extends BaseController{
      * This method checks the available products in Cart
      * @throws SQLException
      * data coupling
+     * Phân tích tính Conhesion:
+     * - Functional Cohesion: thực hiện một nhiệm vụ cụ thể là kiểm tra xem sản phẩm có trong giỏ hàng hay không.
      */
     public void checkAvailabilityOfProduct() throws SQLException{
         Cart.getCart().checkAvailabilityOfProduct();
@@ -24,6 +26,9 @@ public class ViewCartController extends BaseController{
      * This method calculates the cart subtotal
      * @return subtotal
      * data coupling
+     * Phân tích tính Conhesion:
+     * - Functional Cohesion: thực hiện chức năng tính toán giỏ hàng.
+     * - Informational Cohesion: Trả về số lượng sản phẩm trong giỏ hàng.
      */
     public int getCartSubtotal(){
         int subtotal = Cart.getCart().calSubtotal();
